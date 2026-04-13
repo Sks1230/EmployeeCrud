40e2dc3b2c8e4771cf59d213d517c394fd4acb99
Sks1230 <navalcommand2492002@gmail.com>
2026-04-13 15:13:02 +0530

    Fix tests: use H2 with test profile, bypass MySQL dependency

--- EmployeeApiCrud/src/test/java/com/example/EmployeeApiCrud/EmployeeApiCrudApplicationTests.java
+++ EmployeeApiCrud/src/test/java/com/example/EmployeeApiCrud/EmployeeApiCrudApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.EmployeeApiCrud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmployeeApiCrudApplicationTests {
 
 	@Test
